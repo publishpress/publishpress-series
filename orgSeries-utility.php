43fe2dea34338b2702aacf01cf65756bf03226ae
nerrad <nerrad@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-26 02:24:17 +0000

    - fixed more token replace bugs
    git-svn-id: http://plugins.svn.wordpress.org/organize-series/trunk@304335 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- orgSeries-utility.php
+++ orgSeries-utility.php
@@ -120,7 +120,7 @@ function token_replace($replace, $referral = 'other', $id = 0, $ser_ID = 0) {
 	if( stristr($replace, '%previous_post_custom%') ) 
 	$replace = str_replace('%previous_post_custom%', wp_series_nav($id, FALSE, TRUE), $replace);
 	
-	$replace = apply_filters('post_orgseries_token_replace', $replace, $referral, $id, $ser_id);
+	$replace = apply_filters('post_orgseries_token_replace', $replace, $referral, $id, $p_id, $ser_id);
 	return $replace;
 }
 
