75394b1f943f7d7a26ef6bcf7f979e40e668896c
Darren Ethier <darren@roughsmootheng.in>
2012-06-18 11:01:11 -0400

    [completed: #15]

--- orgSeries.php
+++ orgSeries.php
@@ -18,7 +18,7 @@ $os_version = '2.4';
 ######################################
 // Organize Series Wordpress Plugin
 //
-//"Organize Series Plugin" is copyright (c) 2007-2011 Darren Ethier. This program is free software; you can redistribute it and/or
+//"Organize Series Plugin" is copyright (c) 2007-2012 Darren Ethier. This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
