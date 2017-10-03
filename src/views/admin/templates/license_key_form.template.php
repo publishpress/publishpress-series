cea8577e7b240104531940c4d51323bfdabccc4c
Darren Ethier <darren@roughsmootheng.in>
2017-10-02 20:17:24 -0400

    styling and finagling of the license key form #29

--- src/views/admin/templates/license_key_form.template.php
+++ src/views/admin/templates/license_key_form.template.php
@@ -16,8 +16,8 @@ $key = $license_key->getLicenseKey();
             esc_html__('License Key for %1$s', 'organize-series'), $license_key->getItemName()
         ); ?>
     </h3>
+    <label class="description" for="os-license-key-<?php echo $extension_slug; ?>"><?php esc_html_e('Enter your license key: ', 'organize-series'); ?></label>
     <input id="os-license-key-<?php echo $extension_slug; ?>" type="text" name="os_license_key_<?php echo $extension_slug; ?>" class="regular-text" value="<?php echo $key; ?>">
-    <label class="description" for="os-license-key-<?php echo $extension_slug; ?>"><?php esc_html_e('Enter your license key', 'organize-series'); ?></label>
     <?php if (! empty($key)) : ?>
     <div class="os-license-key-meta">
         <p>
@@ -25,10 +25,12 @@ $key = $license_key->getLicenseKey();
         </p>
     </div>
     <?php endif;?>
-    <?php wp_nonce_field('os_license_key_nonce_' . $extension_slug, 'os_license_key_nonce_' . $extension_slug); ?>
-    <?php if ($license_key->getStatus() === 'valid') : ?>
-        <input data-extension="<?php echo $extension_slug; ?>" type="submit" class="button-secondary deactivation-button js-license-submit" name="os_license_key_deactivate" value="<?php esc_html_e('Deactivate License', 'organize-series'); ?>">
-    <?php else: ?>
-        <input data-extension="<?php echo $extension_slug; ?>" type="submit" class="button-secondary activation-button js-license-submit" name="os_license_key_activate" value="<?php esc_html_e('Activate License', 'organize-series'); ?>">
-    <?php endif; ?>
+    <div class="license-key-submit-button">
+        <?php wp_nonce_field('os_license_key_nonce_' . $extension_slug, 'os_license_key_nonce_' . $extension_slug); ?>
+        <?php if ($license_key->getStatus() === 'valid') : ?>
+            <input data-extension="<?php echo $extension_slug; ?>" type="submit" class="button-secondary deactivation-button js-license-submit" name="os_license_key_deactivate" value="<?php esc_html_e('Deactivate License', 'organize-series'); ?>">
+        <?php else: ?>
+            <input data-extension="<?php echo $extension_slug; ?>" type="submit" class="button-secondary activation-button js-license-submit" name="os_license_key_activate" value="<?php esc_html_e('Activate License', 'organize-series'); ?>">
+        <?php endif; ?>
+    </div>
 </div>
