8feede07cf910eba745352c8ae8e107dd7cc0d32
Darren Ethier <darren@roughsmootheng.in>
2018-04-07 11:25:23 -0400

    fix feed address

--- orgSeries-feed.php
+++ orgSeries-feed.php
@@ -15,7 +15,7 @@ $size = 5;
 $date = false;
 //let's set the cache time for simple pie
 add_filter('wp_feed_cache_transient_lifetime', create_function( '$a', "return $cache_time;" ) );
-$rss = fetch_feed('http://www.organizeseries.com/category/blog/feed/');
+$rss = fetch_feed('http://organizeseries.com/category/blog/feed/');
 ?>
 		<p><?php _e('The following will keep you updated with all the recent Organize Series Plugin related news by <a href="http://unfoldingneurons.com" title="Visit Darren Ethier\'s blog">Darren Ethier</a>.', 'organize-series'); ?></p>
 <?php
