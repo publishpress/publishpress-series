ca38c726dc55446ead26210c9a889148353b5aef
Olawale Adesina <olawale.olatech.pro@gmail.com>
2021-08-11 16:42:53 +0100

    - Clean up sidebar box in post editigg screen #99

--- js/series.js
+++ js/series.js
@@ -1,15 +1,15 @@
 jQuery(document).ready( function($) {
 	var myConfirm = function() { return '' !== $('#newseries').val(); };
-	
-	$('#jaxseries').prepend('<span id="ajaxseries"><input type="text" name="newseries" id="newseries" size="16" autocomplete="off"/><input type="button" name="Button" class="add:serieschecklist:jaxseries" id="seriesadd" value="' + seriesL10n.add + '" /><input type="hidden"/><input type="hidden"/><span id="howto">' + seriesL10n.how + '</span></span><span id="series-ajax-response"></span><span id="add-series-nonce" class="hidden">' + seriesL10n.addnonce + '</span>');
-	
+
+	$('#jaxseries').prepend('<span id="ajaxseries"><input type="text" name="newseries" id="newseries" size="16" autocomplete="off"/><input type="button" name="Button" class="add:serieschecklist:jaxseries button" id="seriesadd" value="' + seriesL10n.add + '" /><input type="hidden"/><input type="hidden"/><span id="howto" class="howto">' + seriesL10n.how + '</span></span><span id="series-ajax-response"></span><span id="add-series-nonce" class="hidden">' + seriesL10n.addnonce + '</span>');
+
 	/*$('#serieschecklist').wpList( { alt: '', response: 'series-ajax-response', confirm: myConfirm } ); /**/
-	
-	
+
+
 	$('#seriesadd').click( function() {
 		$('input','#serieschecklist').removeProp('checked');
 		/* console.log($('#newseries').val()); /**/
-		
+
 		var data = {
 			action: 'add_series',
 			newseries: $('#newseries').val(),
@@ -29,7 +29,7 @@ jQuery(document).ready( function($) {
 			}
 		});
 	});
-	
+
 	$('#seriesadd').mouseup( function() {
 		setTimeout( function() {
 			var new_id = $('input:checked', '#serieschecklist').val();
@@ -37,10 +37,10 @@ jQuery(document).ready( function($) {
 				$('#series_part').attr('name', 'series_part['+new_id+']')
 		}, 1000);
 	});
-	
+
 	$('input[name|="post_series"]').click(function() {
 		var ser_id = $(this).attr('id').replace('in-series-', '');
 		$('#series_part').attr('name', 'series_part['+ser_id+']')
 	});
-	
+
 } );
