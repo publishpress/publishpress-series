6d6d2be0cf08058458c83290a5e2225ccbb84001
Olawale Adesina <olawale.olatech.pro@gmail.com>
2022-01-19 19:34:21 +0100

    - Add release build file

---
+++ RoboFile.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This is project's console commands configuration for Robo task runner.
+ *
+ * @see http://robo.li/
+ */
+class RoboFile extends \PublishPressBuilder\PackageBuilderTasks
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->appendToFileToIgnore(
+            [
+                '.phplint.yml',
+                'webpack.config.js',
+                'vendor/pimple/pimple/.gitignore',
+                'vendor/pimple/pimple/.php_cs.dist',
+                'vendor/psr/container/.gitignore',
+                'vendor/publishpress/wordpress-version-notices/.gitignore',
+                'vendor/publishpress/wordpress-version-notices/README.md',
+                'vendor/publishpress/wordpress-version-notices/bin',
+                'vendor/publishpress/wordpress-version-notices/codeception.dist.yml',
+                'vendor/publishpress/wordpress-version-notices/codeception.yml',
+                'vendor/publishpress/wordpress-version-notices/tests',
+            ]
+        );
+    }
+}
