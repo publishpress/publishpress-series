f65841fb732cc74190e8d0c9d08b99dabf0b4ab0
Olawale Adesina <olawale.olatech.pro@gmail.com>
2022-03-02 15:30:25 +0100

    - Add Pro upgrade panels to Free #407

---
+++ includes-core/pro-placeholder/views/cpt-support-placeholder.php
@@ -0,0 +1,22 @@
+<div class="wrap">
+    <h1 class="wp-heading-inline"><?php esc_html_e('PublishPress Series Plugin Options', 'organize-series'); ?></h1>
+    <hr class="wp-header-end">
+    <div id="pp-series-menu-wrapper" class="postbox">
+        <div class="pp-series-menus-promo">
+            <div class="pp-series-menus-promo-inner">
+                <img src="<?php echo esc_url(SERIES_PATH_URL . 'includes-core/pro-placeholder/assets/images/cpt-support.jpg'); ?>" class="pp-series-desktop">
+                <img src="<?php echo esc_url(SERIES_PATH_URL . 'includes-core/pro-placeholder/assets/images/cpt-support-mobile.jpg'); ?>" class="pp-series-mobile">
+                <div class="pp-series-menus-promo-content">
+                    <p>
+                        <?php esc_html_e('PublishPress Series pro adds support for enabling series usage with custom post types.', 'organize-series'); ?>
+                    </p>
+                    <p>
+                        <a href="https://publishpress.com/links/series-banner" target="_blank">
+                            <?php esc_html_e('Upgrade to Pro', 'organize-series'); ?>
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
