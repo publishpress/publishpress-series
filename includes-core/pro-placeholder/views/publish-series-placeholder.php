7f2bbef23bad1313819fdaa0085f5a463cd5f617
Olawale Adesina <olawale.olatech.pro@gmail.com>
2022-03-15 06:39:06 +0100

    - Upgrade Pro background image #460

--- includes-core/pro-placeholder/views/publish-series-placeholder.php
+++ includes-core/pro-placeholder/views/publish-series-placeholder.php
@@ -4,8 +4,8 @@
     <div id="pp-series-menu-wrapper" class="postbox">
         <div class="pp-series-menus-promo">
             <div class="pp-series-menus-promo-inner">
-                <img src="<?php echo esc_url(SERIES_PATH_URL . 'includes-core/pro-placeholder/assets/images/publish-series.jpg'); ?>" class="pp-series-desktop">
-                <img src="<?php echo esc_url(SERIES_PATH_URL . 'includes-core/pro-placeholder/assets/images/publish-series-mobile.jpg'); ?>" class="pp-series-mobile">
+                <img src="<?php echo esc_url(SERIES_PATH_URL . 'includes-core/pro-placeholder/assets/images/publish-series-addon.jpg'); ?>" class="pp-series-desktop">
+                <img src="<?php echo esc_url(SERIES_PATH_URL . 'includes-core/pro-placeholder/assets/images/publish-series-addon-mobile.jpg'); ?>" class="pp-series-mobile">
                 <div class="pp-series-menus-promo-content">
                     <p>
                         <?php esc_html_e('PublishPress Series Pro enables easy bulk publishing of all posts in a series at once.', 'organize-series'); ?>
