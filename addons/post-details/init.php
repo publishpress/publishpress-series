a075b4b243aef9958217ebe8198dde3f765a24a2
Riza Maulana Ardiyanto <rizaardiyanto01@gmail.com>
2026-03-26 14:55:05 +0700

    Keep series shortcodes active when display toggles are disabled

--- addons/post-details/init.php
+++ addons/post-details/init.php
@@ -10,12 +10,6 @@ if (! defined('ABSPATH')) {
 // Always load utilities (needed by settings page)
 require_once __DIR__ . '/includes/class-utilities.php';
 
-// Skip everything else if the Post Details toggle is off
-$pps_pd_opts = get_option('org_series_options', []);
-if (empty($pps_pd_opts['auto_tag_seriesmeta_toggle'])) {
-    return;
-}
-
 require_once __DIR__ . '/post-details.php';
 require_once __DIR__ . '/includes/class-fields.php';
 require_once __DIR__ . '/classes/SeriesPostDetailsRenderer.php';
